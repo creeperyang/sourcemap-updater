f50f5fd01b6dfb40f7d04b52fc596da94fa0fd11
creeperyang <yangcreeper@hotmail.com>
2017-01-05 23:41:35 +0800

    finish main function

---
+++ test/index.spec.js
@@ -0,0 +1,49 @@
+const { join } = require('path')
+const { readFileSync } = require('fs')
+const test = require('ava')
+const update = require('../lib')
+
+test('should generate correct source map when css not changes.', t => {
+    const content = readFileSync(`${__dirname}/res/concated.css`)
+    const res = update([
+        content,
+        readFileSync(`${__dirname}/res/concated.css.map`)
+    ])
+    t.is(res.css, content.toString())
+    t.deepEqual(res.map, require('./res/concated.expected.json'))
+})
+
+test('should generate correct source map when css changes.', t => {
+    const res = update([
+        readFileSync(`${__dirname}/res/concated.css`),
+        readFileSync(`${__dirname}/res/concated.css.map`)
+    ], {
+        updater(str, node, type) {
+            // for `prop-value`s, remove all newline of value.
+            if (!type && node && node.prop) {
+                node.value = node.value.replace(/\s*\n\s*/g, '')
+                return node.toString() + ';'
+            }
+            return false
+        }
+    })
+    t.is(res.css, readFileSync(`${__dirname}/res/concated.beautiful.css`).toString())
+    t.deepEqual(res.map, require('./res/concated.beautiful.expected.json'))
+})
+
+test('should generate correct source map when css deletes some lines.', t => {
+    const res = update([
+        readFileSync(`${__dirname}/res/concated.css`),
+        readFileSync(`${__dirname}/res/concated.css.map`)
+    ], {
+        updater(str, node, type) {
+            // for `prop-value`s, remove all newline of value.
+            if (!type && node && node.prop === 'background-image') {
+                return ''
+            }
+            return false
+        }
+    })
+    t.is(res.css, readFileSync(`${__dirname}/res/concated.nobg.css`).toString())
+    t.deepEqual(res.map, require('./res/concated.nobg.expected.json'))
+})
