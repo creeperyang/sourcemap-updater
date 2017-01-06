bc1b37a67d26bcd38dd148fe8ccbde3fa9de07f4
creeperyang <yangcreeper@hotmail.com>
2017-01-06 20:58:53 +0800

    support empty source map, close #1

--- test/index.spec.js
+++ test/index.spec.js
@@ -46,3 +46,42 @@ test('should generate correct source map when css deletes some lines.', t => {
     t.is(res.css, readFileSync(`${__dirname}/res/concated.nobg.css`).toString())
     t.deepEqual(res.map, require('./res/concated.nobg.expected.json'))
 })
+
+test('should generate correct source map when map is empty.', t => {
+    const content = readFileSync(`${__dirname}/res/concated.css`)
+    const expected = require('./res/raw.json')
+
+    const res = update([
+        content,
+        null
+    ])
+    t.is(res.css, content.toString())
+    t.deepEqual(res.map, expected)
+
+    const res2 = update([
+        content,
+        {}
+    ])
+    t.is(res2.css, content.toString())
+    t.deepEqual(res2.map, expected)
+
+    const res3 = update([
+        content,
+        {}
+    ], { file: 'css/y.css' })
+    t.is(res3.css, content.toString())
+    t.deepEqual(res3.map, Object.assign({}, expected, {
+        file: 'css/y.css',
+        sources: ['css/y.css']
+    }))
+
+    const res4 = update([
+        content,
+        { file: 'css/y.css' }
+    ])
+    t.is(res4.css, content.toString())
+    t.deepEqual(res4.map, Object.assign({}, expected, {
+        file: 'css/y.css',
+        sources: ['css/y.css']
+    }))
+})
